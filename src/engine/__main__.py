9904a4c67967ded10415dd4eee5ac236d8afa863
Udit <udit.samant@gmail.com>
2025-06-26 05:35:14 +1000

    Add basic engine structure

--- src/engine/__main__.py
+++ src/engine/__main__.py
@@ -1,5 +1,21 @@
+from signal import SIGTERM, signal
+import sys
+
+from engine.game_engine import GameEngine
+
+
 def main():
-    print("Running the engine!")
+    engine = GameEngine()
+
+    def kill_handler(a, b):
+        # TODO
+        assert False
+        sys.exit()
+
+    signal(SIGTERM, kill_handler)
+
+    engine.run()
+
 
 if __name__ == "__main__":
     main()
--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -1,5 +1,12 @@
+from engine.state.game_state import GameState
+
 class GameEngine:
     def __init__(self):
+        print("Intialising game engine!")
+
         self.state = GameState()
         # Logging
         # Output
+
+    def run(self):
+        print("Game Engine Live!")
