b56ffd153933bae355cb6a1e4d722560ea35e60e
Udit <udit.samant@gmail.com>
2025-06-26 10:59:44 +1000

    Add engine skeleton

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -1,4 +1,6 @@
+from engine.config.game_config import MAX_ROUNDS
 from engine.state.game_state import GameState
+
 
 class GameEngine:
     def __init__(self):
@@ -9,4 +11,13 @@ class GameEngine:
         # Output
 
     def run(self):
-        print("Game Engine Live!")
+        # turn_order = list(self.state.players)
+
+        while not self.state.is_game_over():
+            print(f"New round {self.state.round + 1}")
+            self.state.start_new_round
+
+            if self.state.round > MAX_ROUNDS or any(
+                [p >= 50 for p in self.state.get_player_points()]
+            ):
+                self.state.finalise_game()
---
+++ src/engine/state/player_state.py
@@ -0,0 +1,4 @@
+class PlayerState:
+    def __init__(self, player_id: int) -> None:
+        self.id = player_id
+        self.points = 0
