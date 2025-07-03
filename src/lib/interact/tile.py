131ad13b5053c0dc7b111dd1523df2365541bbf1
Udit <udit.samant@gmail.com>
2025-07-03 11:22:20 +1000

    Add tiles A-H

--- src/lib/interact/tile.py
+++ src/lib/interact/tile.py
@@ -1,5 +1,7 @@
 from lib.interact.structure import StructureType
+
 from enum import Enum, auto
+from copy import copy
 
 
 # class NodeType(Enum):
@@ -23,25 +25,29 @@ from enum import Enum, auto
 
 
 class TileModifier(Enum):
+    RIVER = auto()
     MONESTERY = auto()
     SHIELD = auto()
 
 
 class Tile:
-    center: StructureType
+    left_tile: "Tile"
+    right_tile: "Tile"
+    top_tile: "Tile"
+    bottom_tile: "Tile"
 
     def __init__(
         self,
-        left: StructureType,
-        right: StructureType,
-        top: StructureType,
-        bottom: StructureType,
+        left_edge: StructureType,
+        right_edge: StructureType,
+        top_edge: StructureType,
+        bottom_edge: StructureType,
         modifiers: list[TileModifier] = list(),
     ) -> None:
-        self.left = left
-        self.right = right
-        self.top = top
-        self.bottom = bottom
+        self.left_edge = left_edge
+        self.right_edge = right_edge
+        self.top_edge = top_edge
+        self.bottom_edge = bottom_edge
 
         self.roation = 0
 
@@ -49,33 +55,93 @@ class Tile:
 
     def rotate_clockwise(self, number: int) -> None:
         for _ in range(number):
-            self.right, self.bottom, self.left, self.top = (
-                self.top,
-                self.right,
-                self.bottom,
-                self.left,
+            self.right_edge, self.bottom_edge, self.left_edge, self.top_edge = (
+                self.top_edge,
+                self.right_edge,
+                self.bottom_edge,
+                self.left_edge,
             )
 
     @staticmethod
     def get_starting_tile() -> "Tile":
         return Tile(
-            left=StructureType.GRASS,
-            right=StructureType.GRASS,
-            top=StructureType.RIVER,
-            bottom=StructureType.GRASS,
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.RIVER,
+            bottom_edge=StructureType.GRASS,
+            modifiers=[TileModifier.RIVER]
         )
 
+    def clone_add(self, n: int) -> list["Tile"]:
+        cloned_tiles = [copy(self) for i in range n]
+        cloned_tiles.append(self)
+        return cloned_tiles
 
-def create_all_tiles() -> dict[int, Tile]:
+
+def create_river_tiles():
+    pass
+
+def create_regular_tiles():
     tiles = []
 
-    def create_river_tiles():
-        pass
+    # Tile Type A
+    tiles.extend(Tile(
+        left_edge=StructureType.GRASS, 
+        right_edge=StructureType.GRASS, 
+        top_edge=StructureType.GRASS, 
+        bottom_edge=StructureType.ROAD_START).clone_add(2))
 
-    def create_regular_tiles():
-        pass
+    # Tile Type B
+    tiles.extend(Tile(
+        left_edge=StructureType.GRASS, 
+        right_edge=StructureType.GRASS, 
+        top_edge=StructureType.GRASS, 
+        bottom_edge=StructureType.GRASS,
+        modifiers=[TileModifier.MONESTERY]).clone_add(4))
 
-    def create_expansion_tiles():
-        pass
+    # Tile Type C
+    tiles.extend(Tile(
+        left_edge=StructureType.CITY, 
+        right_edge=StructureType.CITY, 
+        top_edge=StructureType.CITY, 
+        bottom_edge=StructureType.CITY,
+        modifiers=[TileModifier.SHIELD]).clone_add(1))
 
-    return dict(tiles)
+    # Tile Type D
+    tiles.extend(Tile(
+        left_edge=StructureType.GRASS, 
+        right_edge=StructureType.CITY, 
+        top_edge=StructureType.ROAD, 
+        bottom_edge=StructureType.ROAD).clone_add(1))
+
+    # Tile Type E
+    tiles.extend(Tile(
+        left_edge=StructureType.GRASS, 
+        right_edge=StructureType.GRASS, 
+        top_edge=StructureType.CITY, 
+        bottom_edge=StructureType.GRASS).clone_add(5))
+
+    # Tile Type F
+    tiles.extend(Tile(
+        left_edge=StructureType.CITY, 
+        right_edge=StructureType.CITY, 
+        top_edge=StructureType.GRASS, 
+        bottom_edge=StructureType.GRASS,
+        modifiers=[TileModifier.SHIELD]).clone_add(2))
+
+    # Tile Type G
+    tiles.extend(Tile(
+        left_edge=StructureType.GRASS, 
+        right_edge=StructureType.GRASS, 
+        top_edge=StructureType.CITY, 
+        bottom_edge=StructureType.CITY).clone_add(2))
+
+    # Tile Type H
+    tiles.extend(Tile(
+        left_edge=StructureType.GRASS, 
+        right_edge=StructureType.GRASS, 
+        top_edge=StructureType.CITY, 
+        bottom_edge=StructureType.CITY).clone_add(3))
+
+def create_expansion_tiles():
+    pass
