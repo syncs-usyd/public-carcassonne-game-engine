ed55ae2853c94c0c79c755ee82480812c6a68fb4
Udit <udit.samant@gmail.com>
2025-07-03 12:19:58 +1000

    Add all base tiles w/o edges

--- src/lib/interact/tile.py
+++ src/lib/interact/tile.py
@@ -3,6 +3,8 @@ from lib.interact.structure import StructureType
 from enum import Enum, auto
 from copy import copy
 
+from typing import final, Self
+
 
 # class NodeType(Enum):
 #     CENTER = auto()
@@ -25,16 +27,34 @@ from copy import copy
 
 
 class TileModifier(Enum):
+    """
+    TileModifier
+    Desc: _A multi-strategy based deisgn modifier for tile_
+    Motivated by two reasons
+        - My Laziness
+        - To allow for larger flexibility of bot algorithms
+    """
+
     RIVER = auto()
     MONESTERY = auto()
     SHIELD = auto()
+    BROKEN_ROAD_CENTER = auto()
 
 
 class Tile:
+    """
+    Tile
+    Desc: _Stores all Tile Info by internal edges (Structures) and external connections_
+    """
+
     left_tile: "Tile"
     right_tile: "Tile"
     top_tile: "Tile"
     bottom_tile: "Tile"
+
+    # Allows for connected componentes for structures within a tile
+    # For example: differenctiates between a three edge city and three distinct cities as edges
+    structure_edges: list["StructureType"]
 
     def __init__(
         self,
@@ -69,11 +89,12 @@ class Tile:
             right_edge=StructureType.GRASS,
             top_edge=StructureType.RIVER,
             bottom_edge=StructureType.GRASS,
-            modifiers=[TileModifier.RIVER]
+            modifiers=[TileModifier.RIVER],
         )
 
-    def clone_add(self, n: int) -> list["Tile"]:
-        cloned_tiles = [copy(self) for i in range n]
+    @final
+    def clone_add(self, n: int) -> list[Self]:
+        cloned_tiles = [copy(self) for i in range(n)]
         cloned_tiles.append(self)
         return cloned_tiles
 
@@ -81,67 +102,259 @@ class Tile:
 def create_river_tiles():
     pass
 
+
 def create_regular_tiles():
-    tiles = []
+    tiles: list["Tile"] = []
 
     # Tile Type A
-    tiles.extend(Tile(
-        left_edge=StructureType.GRASS, 
-        right_edge=StructureType.GRASS, 
-        top_edge=StructureType.GRASS, 
-        bottom_edge=StructureType.ROAD_START).clone_add(2))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.GRASS,
+            bottom_edge=StructureType.ROAD_START,
+        ).clone_add(2)
+    )
 
     # Tile Type B
-    tiles.extend(Tile(
-        left_edge=StructureType.GRASS, 
-        right_edge=StructureType.GRASS, 
-        top_edge=StructureType.GRASS, 
-        bottom_edge=StructureType.GRASS,
-        modifiers=[TileModifier.MONESTERY]).clone_add(4))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.GRASS,
+            bottom_edge=StructureType.GRASS,
+            modifiers=[TileModifier.MONESTERY],
+        ).clone_add(4)
+    )
 
     # Tile Type C
-    tiles.extend(Tile(
-        left_edge=StructureType.CITY, 
-        right_edge=StructureType.CITY, 
-        top_edge=StructureType.CITY, 
-        bottom_edge=StructureType.CITY,
-        modifiers=[TileModifier.SHIELD]).clone_add(1))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.CITY,
+            modifiers=[TileModifier.SHIELD],
+        ).clone_add(1)
+    )
 
     # Tile Type D
-    tiles.extend(Tile(
-        left_edge=StructureType.GRASS, 
-        right_edge=StructureType.CITY, 
-        top_edge=StructureType.ROAD, 
-        bottom_edge=StructureType.ROAD).clone_add(1))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.ROAD,
+            bottom_edge=StructureType.ROAD,
+        ).clone_add(1)
+    )
 
     # Tile Type E
-    tiles.extend(Tile(
-        left_edge=StructureType.GRASS, 
-        right_edge=StructureType.GRASS, 
-        top_edge=StructureType.CITY, 
-        bottom_edge=StructureType.GRASS).clone_add(5))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.GRASS,
+        ).clone_add(5)
+    )
 
     # Tile Type F
-    tiles.extend(Tile(
-        left_edge=StructureType.CITY, 
-        right_edge=StructureType.CITY, 
-        top_edge=StructureType.GRASS, 
-        bottom_edge=StructureType.GRASS,
-        modifiers=[TileModifier.SHIELD]).clone_add(2))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.GRASS,
+            bottom_edge=StructureType.GRASS,
+            modifiers=[TileModifier.SHIELD],
+        ).clone_add(2)
+    )
 
     # Tile Type G
-    tiles.extend(Tile(
-        left_edge=StructureType.GRASS, 
-        right_edge=StructureType.GRASS, 
-        top_edge=StructureType.CITY, 
-        bottom_edge=StructureType.CITY).clone_add(2))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.CITY,
+        ).clone_add(2)
+    )
 
     # Tile Type H
-    tiles.extend(Tile(
-        left_edge=StructureType.GRASS, 
-        right_edge=StructureType.GRASS, 
-        top_edge=StructureType.CITY, 
-        bottom_edge=StructureType.CITY).clone_add(3))
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.GRASS,
+            bottom_edge=StructureType.GRASS,
+        ).clone_add(3)
+    )
+
+    # Tile Type I
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.GRASS,
+            bottom_edge=StructureType.CITY,
+        ).clone_add(2)
+    )
+
+    # Tile Type J
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.ROAD,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.ROAD,
+        ).clone_add(3)
+    )
+
+    # Tile Type K
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.ROAD,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.ROAD,
+            bottom_edge=StructureType.GRASS,
+        ).clone_add(3)
+    )
+
+    # Tile Type L
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.ROAD_START,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.ROAD_START,
+            bottom_edge=StructureType.ROAD_START,
+        ).clone_add(3)
+    )
+
+    # Tile Type M
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.GRASS,
+            modifiers=[TileModifier.SHIELD],
+        ).clone_add(2)
+    )
+
+    # Tile Type N
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.GRASS,
+        ).clone_add(3)
+    )
+
+    # Tile Type O
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.ROAD,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.ROAD,
+            modifiers=[TileModifier.SHIELD],
+        ).clone_add(2)
+    )
+
+    # Tile Type P
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.ROAD,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.ROAD,
+        ).clone_add(3)
+    )
+
+    # Tile Type Q
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.GRASS,
+            modifiers=[TileModifier.SHIELD],
+        ).clone_add(1)
+    )
+
+    # Tile Type R
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.GRASS,
+        ).clone_add(3)
+    )
+
+    # Tile Type S
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.ROAD,
+            modifiers=[TileModifier.SHIELD],
+        ).clone_add(2)
+    )
+
+    # Tile Type T
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.CITY,
+            right_edge=StructureType.CITY,
+            top_edge=StructureType.CITY,
+            bottom_edge=StructureType.ROAD,
+        ).clone_add(1)
+    )
+
+    # Tile Type U
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.GRASS,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.ROAD,
+            bottom_edge=StructureType.ROAD,
+        ).clone_add(8)
+    )
+
+    # Tile Type V
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.ROAD,
+            right_edge=StructureType.GRASS,
+            top_edge=StructureType.GRASS,
+            bottom_edge=StructureType.ROAD,
+        ).clone_add(9)
+    )
+
+    # Tile Type W
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.ROAD_START,
+            right_edge=StructureType.ROAD_START,
+            top_edge=StructureType.GRASS,
+            bottom_edge=StructureType.ROAD_START,
+            modifiers=[TileModifier.BROKEN_ROAD_CENTER],
+        ).clone_add(4)
+    )
+
+    # Tile Type X
+    tiles.extend(
+        Tile(
+            left_edge=StructureType.ROAD_START,
+            right_edge=StructureType.ROAD_START,
+            top_edge=StructureType.ROAD_START,
+            bottom_edge=StructureType.ROAD_START,
+            modifiers=[TileModifier.BROKEN_ROAD_CENTER],
+        ).clone_add(1)
+    )
+
 
 def create_expansion_tiles():
     pass
