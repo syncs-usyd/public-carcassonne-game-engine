6cfc52aed342f60572e48276f8cd3937a9422eeb
Udit <udit.samant@gmail.com>
2025-07-09 12:34:55 +1000

    Start client state, and complete all event type mutators

--- src/helper/client_state.py
+++ src/helper/client_state.py
@@ -1,1 +1,41 @@
+from lib.config.map_config import MAX_MAP_LENGTH
+from lib.interact.meeple import Meeple
+from lib.interact.map import Map
+from lib.interface.events.typing import EventType
+from lib.models.player_model import PlayerModel, PublicPlayerModel
 
+
+class ClientSate:
+    def __init__(self):
+        self.round = -1
+        self.players: dict[int, PublicPlayerModel]
+        self.map = Map()
+
+        self.game_over = False
+        self.num_placed_cards = 0
+
+        self.event_history: list[EventType] = []
+        self.new_events: int = 0
+        self.turn_order: list[int] = []
+
+        self.points = 0
+        self.me = PlayerModel()
+
+    def get_meeples_placed_by(self, player_id: int | None) -> list[Meeple]:
+        """
+        Get Meeples Placed
+        Giving None as player id retruns all placed meeples
+        """
+        meeples = []
+        for x in range(MAX_MAP_LENGTH):
+            for y in range(MAX_MAP_LENGTH):
+                tile = self.map._grid[y][x]
+
+                if tile is None:
+                    continue
+
+                for edge, meeple in tile.internal_claims.items():
+                    if meeple and (meeple.player_id == player_id or not player_id):
+                        meeples.append(meeple)
+
+        return meeples
---
+++ src/helper/state_mutator.py
@@ -0,0 +1,145 @@
+from helper.client_state import ClientSate
+
+from lib.interface.events.base_event import BaseEvent
+from lib.interface.events.event_meeple_placed import EventMeeplePlaced
+from lib.interface.events.event_player_bannned import EventPlayerBanned
+from lib.interface.events.event_player_turn_started import EventPlayerTurnStarted
+from lib.interface.events.event_player_won import EventPlayerWon
+from lib.interface.events.event_river_phase_completed import EventRiverPhaseCompleted
+from lib.interface.events.event_game_ended import (
+    EventGameEndedCancelled,
+    EventGameEndedPointLimitReaced,
+    EventGameEndedStaleMate,
+)
+from lib.interface.events.event_game_started import EventGameStarted
+from lib.interface.events.event_player_drew_cards import (
+    EventPlayerDrewCards,
+    PublicEventPlayerDrewCards,
+)
+from lib.interface.events.event_player_meeple_freed import EventPlayerMeepleFreed
+from lib.interface.events.event_tile_placed import (
+    EventStartingTilePlaced,
+)
+from lib.interface.events.moves.move_place_meeple import (
+    MovePlaceMeeple,
+    MovePlaceMeeplePass,
+)
+from lib.interface.events.moves.move_place_tile import MovePlaceTile
+
+
+class StateMutator:
+    def __init__(self, state: ClientSate):
+        self.state = state
+
+    def commit(self, i: int, event: BaseEvent):
+        if i != len(self.state.event_history):
+            raise RuntimeError("Please send us a discord message with this error log.")
+        self.state.event_history.append(event)
+
+        match event:
+            case EventGameStarted() as e:
+                self._commit_event_game_started(e)
+
+            case EventPlayerDrewCards() as e:
+                self._commit_player_drew_cards(e)
+
+            case EventPlayerMeepleFreed() as e:
+                self._commit_event_player_meeple_freed(e)
+
+            case EventStartingTilePlaced() as e:
+                self._commit_event_starting_tile_placed(e)
+
+            case MovePlaceTile() as e:
+                self._commit_move_place_tile(e)
+
+            case MovePlaceMeeple() as e:
+                self._commit_move_place_meeple(e)
+
+            case MovePlaceMeeplePass() as e:
+                self._commit_move_place_meeple_pass(e)
+
+            case PublicEventPlayerDrewCards() as e:
+                self._commit_opponent_drew_cards(e)
+
+            case EventGameEndedPointLimitReaced() as e:
+                self._commit_event_game_ended_point_limit(e)
+
+            case EventGameEndedStaleMate() as e:
+                self._commit_event_game_ended_stalemate(e)
+
+            case EventGameEndedCancelled() as e:
+                self._commit_event_game_ended_cancelled(e)
+
+            case EventMeeplePlaced() as e:
+                self._commit_event_meeple_placed(e)
+
+            case EventPlayerBanned() as e:
+                self._commit_event_player_banned(e)
+
+            case EventPlayerTurnStarted() as e:
+                self._commit_event_player_turn_started(e)
+
+            case EventPlayerWon() as e:
+                self._commit_event_player_won(e)
+
+            case EventRiverPhaseCompleted() as e:
+                self._commit_event_river_phase_completed(e)
+
+            # case EventStructureCompleted() as e:
+            #     self._commit_event_structure_completed(e)
+
+            case _:
+                raise RuntimeError(f"Unrecognised event: {event}")
+
+    def _commit_player_drew_cards(self, e: EventPlayerDrewCards) -> None:
+        if e.player_id != self.state.me.player_id:
+            raise RuntimeError("Please send us a discord message with this error log.")
+
+        self.state.me.tiles.extend(e.cards)
+
+    def _commit_opponent_drew_cards(self, e: PublicEventPlayerDrewCards) -> None:
+        self.state.players[e.player_id].num_tiles += e.num_cards
+
+    def _commit_event_game_started(self, e: EventGameStarted) -> None:
+        pass
+
+    def _commit_event_player_meeple_freed(self, e: EventPlayerMeepleFreed) -> None:
+        pass
+
+    def _commit_event_starting_tile_placed(self, e: EventStartingTilePlaced) -> None:
+        pass
+
+    def _commit_move_place_tile(self, e: MovePlaceTile) -> None:
+        pass
+
+    def _commit_move_place_meeple(self, e: MovePlaceMeeple) -> None:
+        pass
+
+    def _commit_move_place_meeple_pass(self, e: MovePlaceMeeplePass) -> None:
+        pass
+
+    def _commit_event_game_ended_point_limit(
+        self, e: EventGameEndedPointLimitReaced
+    ) -> None:
+        pass
+
+    def _commit_event_game_ended_stalemate(self, e: EventGameEndedStaleMate) -> None:
+        pass
+
+    def _commit_event_game_ended_cancelled(self, e: EventGameEndedCancelled) -> None:
+        pass
+
+    def _commit_event_meeple_placed(self, e: EventMeeplePlaced) -> None:
+        pass
+
+    def _commit_event_player_banned(self, e: EventPlayerBanned) -> None:
+        pass
+
+    def _commit_event_player_turn_started(self, e: EventPlayerTurnStarted) -> None:
+        pass
+
+    def _commit_event_player_won(self, e: EventPlayerWon) -> None:
+        pass
+
+    def _commit_event_river_phase_completed(self, e: EventRiverPhaseCompleted) -> None:
+        pass
